e9a1b3f0f81b8930b713504def8e22ca2a939c61
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-09-06 22:05:46 +0200

    almost done

--- server.js
+++ server.js
@@ -10,6 +10,53 @@ var urlencodedParser = bodyparser.urlencoded({
   extended: false
 })
 
+var mongojs = require('mongojs');
+ var db = mongojs('itemList', ['itemList']);
+
+app.get('/itemList', function (req, res) {
+  console.log('I received a GET request');
+
+  db.itemList.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/itemList', function (req, res) {
+  console.log(req.body);
+  db.itemList.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/itemList/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.itemList.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/itemList/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.itemList.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/itemList/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.date);
+  db.itemList.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {date: req.body.date, description: req.body.description}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
 app.listen(3000, function(){
   console.log('App is running on port 3000')
 })
--- static/controller/main.js
+++ static/controller/main.js
@@ -1,6 +1,6 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['toastr']);
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl",  ['$scope', '$http', 'toastr',  function($scope, $http, toastr) {
       $scope.collapse = function () {
         $scope.visible = false;
         $scope.visible = $scope.visible = true;
@@ -10,4 +10,65 @@ app.controller("myCtrl", function($scope) {
         $scope.visible = true;
         $scope.visible = $scope.visible = false;
       }
-});
+
+      $scope.collapse3 = function () {
+        $scope.visible = true;
+        $scope.visible = $scope.visible = false;
+      }
+
+      var refresh = function() {
+        $http.get('/itemList').then(function(response) {
+          console.log("I got the data I requested");
+          $scope.items = response.data;
+        });
+      };
+
+      refresh()
+
+      $scope.addItem = function() {
+        console.log($scope.item);
+        $http.post('/itemList', $scope.item).then(function(response) {
+          console.log(response);
+          refresh();
+          $scope.item.date = "";
+          $scope.item.description = "";
+          toastr.success('Item added successfully');
+        });
+      }
+
+      $scope.remove = function(id) {
+          console.log(id);
+          $http.delete('/itemList/' + id).then(function(response) {
+            refresh();
+            toastr.error('Item deleted successfully');
+          });
+      };
+
+      $scope.confirm = function (id){
+        var id = id
+        $scope.visible2 = false;
+        $scope.visible2 = $scope.visible2 = true;
+        $scope.test = id
+      }
+
+      $scope.confirm2 = function (){
+        $scope.visible2 = true;
+        $scope.visible2 = $scope.visible2 = false;
+      }
+
+      $scope.edit = function(id) {
+        console.log(id);
+        $http.get('/itemList/' + id).then(function(response) {
+          $scope.item = response.data;
+        });
+      };
+
+      $scope.update = function() {
+          console.log($scope.item._id);
+          $http.put('/itemList/' + $scope.item._id, $scope.item).then(function(response) {
+            refresh();
+            toastr.success('Item updated successfully');
+          })
+        };
+
+}]);
